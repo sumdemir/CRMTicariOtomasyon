fad1dd480fc4992664961c505ccef46e0d834165
sumdemir <sumdemir2001@gmail.com>
2024-07-11 16:12:44 +0300

    update

---
+++ Controllers/GrafikController.cs
@@ -0,0 +1,102 @@
+﻿using CRMTicariOtomasyon.Models.Siniflar;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace CRMTicariOtomasyon.Controllers
+{
+    public class GrafikController : Controller
+    {
+        // GET: Grafik
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        Context c = new Context();
+        public ActionResult Index2()
+        {
+            ArrayList xvalue = new ArrayList();
+            ArrayList yvalue = new ArrayList();
+            var sonuclar =c.Uruns.ToList();
+            sonuclar.ToList().ForEach(x => xvalue.Add(x.UrunAd));
+            sonuclar.ToList().ForEach(y => yvalue.Add(y.Stok));
+            var grafik = new Chart(width: 500, height: 500)
+                .AddTitle("Stoklar").AddSeries(chartType:"column", name:"Stok",
+                xValue:xvalue, yValues:yvalue);
+            return File(grafik.ToWebImage().GetBytes(),"image/jpeg");
+        }
+
+        public ActionResult Index4()
+        {
+            return View();
+        }
+
+        public ActionResult VisualizeUrunResult() 
+        {
+        
+            return Json(UrunListesi(), JsonRequestBehavior.AllowGet);
+        }
+        
+        public List<sinif1> UrunListesi()
+        {
+            List<sinif1> snf = new List<sinif1>();
+            snf.Add(new sinif1()
+            {
+                urunad = "Bilgisayar",
+                stok = 120
+            });
+            snf.Add(new sinif1()
+            {
+                urunad="Beyaz Eşya",
+                stok=150
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Mobilya",
+                stok = 70
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Küçük Ev Aletleri",
+                stok = 180
+            });
+            snf.Add(new sinif1()
+            {
+                urunad = "Mobil Cihazlar",
+                stok = 90
+            });
+            return snf;
+        }
+
+        public ActionResult Index5() 
+        {
+            return View();
+        }
+        
+        public ActionResult VisualizeUrunResult2()
+        {
+            return Json(UrunListesi2(), JsonRequestBehavior.AllowGet);
+        }
+
+        public List<sinif2> UrunListesi2()
+        {
+            List<sinif2> snf = new List<sinif2>();
+            using (var context = new Context())
+            {
+                snf = c.Uruns.Select(x => new sinif2
+                {
+                    urn = x.UrunAd,
+                    stk = x.Stok
+                }).ToList();
+            }
+            return snf;
+        }
+    }
+    
+    
+}
---
+++ Models/Siniflar/sinif1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRMTicariOtomasyon.Models.Siniflar
+{
+    public class sinif1
+    {
+        public string urunad {  get; set; }
+        public short stok { get; set; }
+
+    }
+}
